6dcd99867c5d5b018f39cf8bb833353198d252a3
Rinx28 <minhanhpreeip@gmail.com>
2024-11-01 02:03:08 +0700

    Update page.tsx

---
+++ src/app/dashboard/[view]/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { useAppSelector } from '@/lib/redux/hook';
+
+export default function Dashboard() {
+	const user = useAppSelector((state) => state.user);
+	return (
+		<div className="flex flex-col items-center justify-center font-protest-strike-regular">
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+			<div className="flex flex-col gap-8 row-start-2 items-center sm:items-start h-96">
+				<h1 className="">Hi {user.name}</h1>
+			</div>
+		</div>
+	);
+}
